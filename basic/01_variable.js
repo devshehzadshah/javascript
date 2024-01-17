ec70df49c982641ac70a61c84047531dc8301cab
shehzad shah <112396234+devshehzadshah@users.noreply.github.com>
2024-01-17 10:59:57 +0000

    variables and data types

---
+++ basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 123225
+let accountEmail = "devshehzad249@gmail.com"
+var accountPassword = "asd1234"
+accountCity = "swabi"
+// accountCity just a assign a space to a varibale it acceptable in javascript but not good practices 
+
+let accountState; 
+// the accountState will be undefiend
+
+// accountId = 3322 not allowed
+/*
+preffer not to use var 
+because of issue in block scop and functional scop
+*/
+accountEmail = "abc@gmail.com"
+accountPassword = "333333333"
+accountCity = "mardan"
+console.table([accountEmail,accountPassword,accountCity,accountState]);
---
+++ basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict ";  // treact all code as a new version
+
+// alert("hello world") we are using nodejs not a browser
+console.log(3 
+    +
+     3); // it will be run but code readability should be high
+
+console.log("shehzad");
+
+let name = "jhon";
+let age = 27;
+let islogedIn = true;
+let state;
+// number 2  to power 53
+// biginit for higher than 2 to power 53
+// string => ""
+// bolean => true or false
+// null => standalone value , empty
+// undefined => the value is not assigned
+// Symbols => unique
+
+// Object
+console.log(typeof null); //type of null is object
+console.log(typeof undefined); // type of undefiend is undefiend
