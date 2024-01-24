5fde21ea6b2c703c5d5e85ebcc247d251aad8fbe
shehzad shah <112396234+devshehzadshah@users.noreply.github.com>
2024-01-24 20:54:30 +0000

    data types operation and comparison

--- basic/03_conversionOperation.js
+++ basic/03_conversionOperation.js
@@ -17,4 +17,28 @@ let BoleanIsLogedIn = Boolean(isLogedIn);
 // "abc" => true
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log( typeof stringNumber);
+//console.log( typeof stringNumber);
+
+// ****************************************** operations *******************************************
+
+let value = 3
+let negValue = - value
+//console.log(negValue);
+// console.log(2+2); 
+// console.log(2-2);
+// console.log(2/2); divison
+// console.log(2*3); multiplication
+// console.log(2**3); power of
+// console.log(15%4); Reminder
+
+let str1 = "Hello"
+let str2 = " jhon"
+let str3 = str1 + str2
+// console.log(str3);
+//console.log(2+2);
+// console.log("2"+2);  if the first value is string all other will be considrd as string 
+// console.log(4+2 + "2"); if first and second value is number the opertaion will be aply on both while the third number result will be as it as "sting " the answer will be 62
+// console.log(+true);
+console.log(+"")
+
+
