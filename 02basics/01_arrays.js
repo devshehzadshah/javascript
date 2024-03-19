b38bb94abb143f2da59b2de022fbf9cd4503507d
shehzad shah <112396234+devshehzadshah@users.noreply.github.com>
2024-03-19 18:44:29 +0000

    array part 1

--- 02basics/01_arrays.js
+++ 02basics/01_arrays.js
@@ -1,13 +1,13 @@
 //  +++++++++++++++++array+++++++++++++++++++
-const myArr = [1,2,3,4,5,];
+// const myArr = [1,2,3,4,5,];
 // console.log(myArr);
 // console.log(myArr[2]);
-const Heros = ['Elon','jhon', 'dov'];
+// const Heros = ['Elon','jhon', 'dov'];
 // console.log(Heros[0]);
-const myArr2 = new Array (1,2,3,4);
+// const myArr2 = new Array (1,2,3,4);
 // console.log("data of myArr2",myArr2[3]);
 // ++++++++++++++++ array methods ++++++++++++++++++++++
- myArr.push(6, 7, 8, 9);
+//  myArr.push(6, 7, 8, 9);
 //  push method are using to add new data to previous array
 //  myArr.pop();
 //  pop method are used to remove last data from array 
@@ -17,6 +17,21 @@ const myArr2 = new Array (1,2,3,4);
 //  shift are used to remove the first data from array
 // console.log(myArr.includes(9));
 // console.log("finding index of my arr", myArr.indexOf(6));
-//  console.log("data added after push", myArr);
-const newArray2 = myArr.join();
-console.log( typeof newArray2);
+//  console.log("data added after push", typeof myArr);
+// const newArray2 = myArr.join();
+// console.log( typeof newArray2);
+// +++++++++++++++++++++ slice and splice ++++++++++++++++++++++++++++++++++++
+// const myArr = [0,1,2,3,4,5]
+// console.log("A ",  myArr);
+// const myn1 = myArr.slice(1,3);
+// console.log("data in myn1 ",myn1);
+// console.log("B ",myArr);
+
+// const myn2 = myArr.splice(1,3);
+// console.log('orignal array ',myn1)
+// console.log("data in myn2 ", myn2);
+// var users = ['ted', 'tem', 'ton', 'sam', 'sor', 'sod']
+// console.log(users.slice(1,4));
+
+// users.slice(1,3, "hi");
+// console.log(new1);
