62c8c5f686ce40a2e2ba61f7e40fa23afcdc2be3
shehzad shah <112396234+devshehzadshah@users.noreply.github.com>
2024-03-19 19:49:42 +0000

    array

--- 02basics/01_arrays.js
+++ 02basics/01_arrays.js
@@ -35,3 +35,23 @@
 
 // users.slice(1,3, "hi");
 // console.log(new1);
+const myArray = [0,1,2,3,4,5]
+// console.log(myArray);
+// myArray.push(6);
+// myArray.pop();
+// myArray.push(7,8)
+// myArray.pop();
+// myArray.unshift(9);
+// myArray.shift();
+// console.log(myArray.includes(3))
+// console.log(myArray.indexOf(2))
+// const mynewArray = myArray.join()
+// console.log( typeof myArray)
+// console.log( typeof mynewArray);
+console.log("A ", myArray);
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B ", myArray)
+const myn2 = myArray.splice(1,3);
+console.log("C ", myArray)
+console.log(myn2);
