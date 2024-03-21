8d12cba172165e62155d01dc67c21037bbed05ef
shehzad shah <112396234+devshehzadshah@users.noreply.github.com>
2024-03-21 18:10:41 +0000

    objects

---
+++ 02basics/03_objects.js
@@ -0,0 +1,15 @@
+// +++++++++ object ++++++++++++++
+// object literals 
+const JsUser = {
+    name : "shehzad",
+    'fullName': "shehzad shah",
+    age : 28,
+    location: "Pakistan",
+    email: "devshehzad249@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "saturday"]
+}
+console.log(JsUser.name)
+console.log(JsUser['age'])
+console.log(JsUser.fullName)
+console.log(JsUser['fullName'])
