9e2d740e926cd81b2ae01fc1ef3be35403378730
shehzad shah <112396234+devshehzadshah@users.noreply.github.com>
2024-03-27 18:52:56 +0000

    objecst in depth

--- 02basics/03_objects.js
+++ 02basics/03_objects.js
@@ -3,7 +3,7 @@
 const mySymbol = Symbol("key1")
 const JsUser = {
     name : "shehzad",
-    [mySymbol] : 'key1',
+    [mySymbol] : 'mykey1',
     'fullName': "shehzad shah",
     age : 28,
     location: "Pakistan",
@@ -11,7 +11,21 @@ const JsUser = {
     isLoggedIn : false,
     lastLoginDays : ["Monday", "saturday"]
 }
-console.log(JsUser.name)
-console.log(JsUser['age'])
-console.log(JsUser.fullName)
-console.log(typeof JsUser[mySymbol])
+// console.log(JsUser.name)
+// console.log(JsUser['age'])
+// console.log(JsUser.fullName)
+// console.log(typeof JsUser[mySymbol])
+JsUser.email = "shehzad@chagpt.com"
+// console.log("updated user email in jsUser object", JsUser['email'])
+// freeze are used for that the object will be never update
+// Object.freeze(JsUser)
+JsUser.name = 'jhon'
+console.log(JsUser)
+JsUser.greeting = function(){
+    console.log('hello from greeting')
+}
+JsUser.greetingTwo = function(){
+    console.log(`helo js user from greeting2 ${this.name}`)
+}
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
